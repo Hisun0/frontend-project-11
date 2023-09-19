5b544a5f1ac2f793f72e2e427e48a10203260db8
Hisun0 <tridvad1@gmail.com>
2023-09-19 11:15:10 +0300

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ttf|png|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+  },
+
+  mode: 'development',
+};
